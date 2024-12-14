c5f86c1f04a6e428f9825bbe855ace88ea589512
Pilvinen <pilvinen@starandserpent.com>
2024-12-14 10:41:09 +0200

    Returned saturation, reduced hunger effect.

--- CriticalFixes/src/main/java/starandserpent/minecraft/criticalfixes/HungerFix.java
+++ CriticalFixes/src/main/java/starandserpent/minecraft/criticalfixes/HungerFix.java
@@ -1,15 +1,19 @@
 package starandserpent.minecraft.criticalfixes;
 
+import org.bukkit.GameMode;
 import org.bukkit.Server;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public class HungerFix implements Listener {
 
     private final JavaPlugin plugin;
     private Server server;
-    private long hungerTimeoutInSeconds = 300L; // 600 = 10 minutes, 300 = 5 minutes.
+    private long hungerTimeoutInSeconds = 450L; // 600 = 10 minutes, 300 = 5 minutes, 450 = 7.5 minutes (2,5h for full hunger)
 
     public HungerFix(JavaPlugin plugin) {
         this.plugin = plugin;
@@ -27,14 +31,19 @@ public class HungerFix implements Listener {
 
     private void tryToMakePlayerHungry(Player player) {
 
-        // If creative, skip
-        if (player.getGameMode() == org.bukkit.GameMode.CREATIVE) {
+        // If not in survival, skip.
+        var playerMode = player.getGameMode();
+        if (playerMode != GameMode.SURVIVAL) {
             return;
         }
 
         // Hunger time.
         var playerHunger = player.getFoodLevel();
-        if (playerHunger > 0) {
+        var maxFoodLevel = 20.0;
+        maxFoodLevel = Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_MAX_HEALTH)).getValue();
+
+        // Reduce hunger if not saturated and not zero.
+        if (playerHunger > 0 && playerHunger < maxFoodLevel) {
             player.setFoodLevel(playerHunger - 1);
         }
     }
