c2f8019162530fe31b8dfc6d136e7a8257b0f5c0
Pilvinen <pilvinen@starandserpent.com>
2024-12-29 02:39:09 +0200

    Improved messaging, fixed a bug.

--- CriticalFixes/src/main/java/starandserpent/minecraft/criticalfixes/BetterRestartServer.java
+++ CriticalFixes/src/main/java/starandserpent/minecraft/criticalfixes/BetterRestartServer.java
@@ -28,19 +28,29 @@ public class BetterRestartServer implements CommandExecutor {
 
             // Check the number of online players
             if (server.getOnlinePlayers().isEmpty()) {
+                // Broadcast the message to Discord
+                String additionalMessage = args.length > 0 ? String.join(" ", args) : "";
+                String message = "Palvelin käynnistyy uudelleen välittömästi. " + additionalMessage;
+                KuuChat.broadcastToDiscord(message);
+
                 // No players online, restart immediately
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "restart");
             }
             else if (server.getOnlinePlayers().size() == 1 && server.getOnlinePlayers().iterator().next().getName().equals("Pilvinen")) {
+                String additionalMessage = args.length > 0 ? String.join(" ", args) : "";
+                String message = "Palvelin käynnistyy uudelleen välittömästi. " + additionalMessage;
+                KuuChat.broadcastToDiscord(message);
+
                 // Only "Pilvinen" is online, restart immediately
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "restart");
             } else {
                 // Construct the message with the parameter
                 String additionalMessage = args.length > 0 ? String.join(" ", args) : "";
-                String[] message = {"Varoitus, ystävät! Palvelin käynnistyy uudelleen 23 sek kuluttua. " + additionalMessage};
+                String message = "Varoitus, ystävät! Palvelin käynnistyy uudelleen 23 sek kuluttua. " + additionalMessage;
+                KuuChat.broadcastToDiscord(message);
 
                 // Send the broadcast message using SystemNotifications
-                SystemNotifications.publicBroadcast(plugin.getServer(), sender, message);
+                SystemNotifications.publicBroadcast(plugin.getServer(), sender, new String[] {message});
 
                 // Schedule the server restart after 23 seconds (460 ticks)
                 Bukkit.getScheduler().runTaskLater(plugin, () -> {
