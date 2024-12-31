b7f2d24015a2a79f47cfab057e938e1c5aac365f
Pilvinen <pilvinen@starandserpent.com>
2024-12-31 03:55:47 +0200

    Added support for decreased odds of banned mobs.

--- CriticalFixes/src/main/java/starandserpent/minecraft/criticalfixes/BannedMobs.java
+++ CriticalFixes/src/main/java/starandserpent/minecraft/criticalfixes/BannedMobs.java
@@ -6,16 +6,22 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
 
 public class BannedMobs implements Listener {
 
     private final JavaPlugin plugin;
     private Server server;
+    private final Random random = new Random();
 
-    // Banned mobs.
-    HashSet<EntityType> bannedMobs = new HashSet<>() {{
-        add(EntityType.PHANTOM);
+    // Banned mobs with their respective ban probabilities (in percentage).
+    Map<EntityType, Integer> bannedMobs = new HashMap<>() {{
+        put(EntityType.PHANTOM, 100);
+        put(EntityType.WANDERING_TRADER, 10);
     }};
 
     public BannedMobs(JavaPlugin plugin) {
@@ -23,12 +29,16 @@ public class BannedMobs implements Listener {
         server = plugin.getServer();
     }
 
-    // Prevent banned mobs from spawning.
-    @EventHandler void onMobSpawn(CreatureSpawnEvent event) {
+    // Prevent banned mobs from spawning based on their ban probability.
+    @EventHandler
+    void onMobSpawn(CreatureSpawnEvent event) {
         var entity = event.getEntity();
         var entityType = entity.getType();
-        if (bannedMobs.contains(entityType)) {
-            event.setCancelled(true);
+        if (bannedMobs.containsKey(entityType)) {
+            int probability = bannedMobs.get(entityType);
+            if (random.nextInt(100) < probability) {
+                event.setCancelled(true);
+            }
         }
     }
 
